cf55909dbfbab8768f00f417489333f155a8a918
Vladosik <100303053+Catshadoll@users.noreply.github.com>
2025-05-24 12:04:42 +0300

    Add files via upload

---
+++ cryptographer.py
@@ -0,0 +1,20 @@
+ALPHABET_RU = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+ALPHABET_EN = "abcdefghijklmnopqrstuvwxyz"
+
+message = input("Введите сообщение: ").lower()
+step = int(input("Введите шаг сдвига: "))
+result = ""
+
+language = input("Выберети язык - RU/EN: ")
+if language == "RU":
+    alphabet = ALPHABET_RU
+elif language == "EN":
+    alphabet = ALPHABET_EN
+else:
+    print("Ошибка: язык должен быть RU или EN")
+
+chipher = [alphabet.find(symbol) + step for symbol in message]
+print(chipher)
+
+decrypted = "".join([alphabet[number - step] for number in chipher])
+print(f"Вы отправили: {decrypted}")
